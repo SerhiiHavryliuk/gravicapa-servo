bab12b5ff2857c41996fc36430754afb8ab2aa39
Serhii H Inv <serhii.havryliuk@eliftech.com>
2024-10-09 08:52:51 +0300

    add calibration and test stand fuunctional

--- gravicapa-servo.ino
+++ gravicapa-servo.ino
@@ -9,7 +9,8 @@
 #include "TFT_eSPI.h"  // Graphics and font library for ST7735 driver chip
 
 // Calibrations ---------------------------------------------------------------
-#include <Calibration.h>                      // Підключаємо бібліотеку
+#include <Calibration.h>                       // Підключаємо бібліотеку
+// todo: калібровку треба завершити, задумка хороша, реалізація ще з костилями
 CalibrationLib calibration(-10, 40, 12, 140);  // Задаємо діапазони температур та кутів
 
 // Use hardware SPI
@@ -18,9 +19,9 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 // TFT Display -----------------------------------------------------------------
 #define TFT_GREY 0x5AEB  // New colour
 unsigned long drawTime = 0;
-int activeItemMenu = 1;    // Активний пункт меню
-int startTemperature = 0;  // Стартова температура
-int deltaTemperature = 5;  // Наростання температури за 1 хв
+int activeItemMenu = 1;      // Активний пункт меню (зараз 2 шт)
+int startTemperature = -10;  // Стартова температураб стартове значення (-10 С)
+int deltaTemperature = 30;   // Наростання температури за 1 хв, стартове значення (30 С)
 // TFT Display -----------------------------------------------------------------
 
 
@@ -28,9 +29,15 @@ int deltaTemperature = 5;  // Наростання температури за 1
 Servo myservo;  // create servo object to control a servo
 // 16 servo objects can be created on the ESP32
 
+// поки не зрозумфло як її використовувати, подумати щоб видалити
 int pos = 0;  // variable to store the servo position
+
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
 int servoPin = 27;
+
+// поки не зрозумфло як її використовувати, подумати щоб видалити
+int currentRotation = 0;     // змінна для відстеження поточного кута
+int startServoPosition = 0;  // змінна для зберігання початкового положення серводвигуна
 // Servo -----------------------------------------------------------------
 
 // Buttons ---------------------------------------------------------------
@@ -43,7 +50,7 @@ struct Button {
 // Кнопки керування меню
 Button buttonReset = { 21, 0, false };
 Button buttonRunTest = { 22, 0, false };
-Button buttonOk = { 17, 0, false };  // поки не використовується
+Button buttonOk = { 17, 0, false };  // todo: поки не використовується, можна використовувати її для встановлення заданої температури для нового алгоритму граничні межі
 Button buttonRight = { 2, 0, false };
 Button buttonLeft = { 15, 0, false };
 Button buttonDown = { 13, 0, false };
@@ -54,6 +61,7 @@ unsigned long button_time = 0;
 unsigned long last_button_time = 0;
 
 // Функції переривань для кнопок
+// дебаунс потрібен щоб не було при одному натисненні деілька спрацювань кнопки
 void IRAM_ATTR isr_buttonReset() {
   button_time = millis();
   if (button_time - last_button_time > 250) {  // Дебаунс 250 мс
@@ -117,7 +125,7 @@ void setup() {
 
   // TFT Display -----------------------------------------------------------------
   tft.init();
-  tft.setRotation(3);                  //  обертання тексту на дисплеї
+  tft.setRotation(3);                  // обертання тексту на дисплеї
   show_loading_menu_display();         // Показуємо заставку при завантаженні
   clear_display();                     // Очищення екрану
   showActiveMenuItem(activeItemMenu);  // Показуємо активний елемент меню
@@ -125,6 +133,7 @@ void setup() {
   // TFT Display -----------------------------------------------------------------
 
   // TFT Buttons ---------------------------------------------------------------
+  // ініціалізація переривань
   pinMode(buttonReset.PIN, INPUT_PULLUP);
   attachInterrupt(buttonReset.PIN, isr_buttonReset, FALLING);
 
@@ -150,38 +159,36 @@ void setup() {
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
-  myservo.setPeriodHertz(50);           // стандартний сигнал 50 Гц
-  myservo.attach(servoPin, 500, 3200);  // підключаємо сервомотор на пін 13
-  goToStartPositionServo();             // Повертаємо сервомотор у початкове положення
+  myservo.setPeriodHertz(50);  // стандартний сигнал 50 Гц
+  myservo.attach(servoPin);    // підключаємо сервомотор на пін 13
+
+  goToStartPositionServo();     // Повертаємо сервомотор у початкове положення, за замовчанням - це -10 С
+
+  // todo: Цей фікс прибирає дрибіжжання сервомотора при старті (якщо стартова температура ще менша), треба розібратись чому не можна виставити в 0 градусів
+  //myservo.write(10);  // повертаємо сервомотор у початкове положення (0 градусів)
   // Servo ---------------------------------------------------------------
 }
 
 void loop() {
-  // int currentTemperature = 25;  // Поточна температура
-  // int rotation = calibrator.mapTemperatureToRotation(currentTemperature);
-
-  // Serial.print("Для температури: ");
-  // Serial.print(currentTemperature);
-  // Serial.print("°C, кут повороту двигуна: ");
-  // Serial.println(rotation);
-  // delay(1000);
-
   // Перевірка натискання кнопок
   // Кнопка Reset
+  // Повертаємо сервомотор у початкове положення
   if (buttonReset.pressed) {
     Serial.printf("Кнопка Reset натискалась %u раз(и)\n", buttonReset.numberKeyPresses);
     buttonReset.pressed = false;
-    goToStartPositionServo();  // Повертаємо сервомотор у початкове положення
+    goToStartPositionServo();  
   }
 
   // Кнопка RunTes
+  // Запускаємо тест із заданими параметрами - вкл сервомотора та поступово рухаємо його
   if (buttonRunTest.pressed) {
     Serial.printf("Кнопка Run Test натискалась %u раз(и)\n", buttonRunTest.numberKeyPresses);
     buttonRunTest.pressed = false;
-    runTestServo(startTemperature, deltaTemperature);  // Запускаємо тест - вкл сервомотора
+    runTestServo(startTemperature, deltaTemperature);  
   }
 
   // Кнопка RunRight
+  // збільшення значення у пункті меню
   if (buttonRight.pressed) {
     Serial.printf("Кнопка Right натискалась %u раз(и)\n", buttonRight.numberKeyPresses);
     buttonRight.pressed = false;
@@ -189,6 +196,7 @@ void loop() {
   }
 
   // Кнопка RunLeft
+  // зменшення значення у пункті меню
   if (buttonLeft.pressed) {
     Serial.printf("Кнопка Left натискалась %u раз(и)\n", buttonLeft.numberKeyPresses);
     buttonLeft.pressed = false;
@@ -196,17 +204,19 @@ void loop() {
   }
 
   // Кнопка RunDown
+  // перехід по меню вниз
   if (buttonDown.pressed) {
     Serial.printf("Кнопка Down натискалась %u раз(и)\n", buttonDown.numberKeyPresses);
     buttonDown.pressed = false;
-    setActiveItemMenu(2);  // Встановлюємо активний пункт в меню
+    setActiveItemMenu(2);  
   }
 
   // Кнопка RunUp
+  // перехід по меню вгору
   if (buttonUp.pressed) {
     Serial.printf("Кнопка Up натискалась %u раз(и)\n", buttonUp.numberKeyPresses);
     buttonUp.pressed = false;
-    setActiveItemMenu(1);  // Встановлюємо активний пункт в меню
+    setActiveItemMenu(1);
   }
 }
 
@@ -214,81 +224,73 @@ void loop() {
 // Запуск тесту
 // -------------------------------------------------------------------------------
 void runTestServo(int startTemp, int deltaTemp) {
-  int servoAngelStartTemp = calibration.getMinRotation();   // Стартовий кут повороту сервомотора сервомотора
-  int servoTimeDeltaTemp = 100;  // Час між кроками сервомотора
-  int testTime = 2;              // Час тесту (2хв, 3хв, 5хв, 15хв)
+  int servoAngelStartTemp = calibration.getMinRotation();  // Стартовий кут повороту сервомотора сервомотора
+  int servoTimeDeltaTemp = 1500;                            // Час між кроками сервомотора
+  int testTime = 2;                                        // Час тесту (2хв, 3хв, 5хв, 15хв)
   int maxRotation = calibration.getMaxRotation();
+
+  // Потрібно для дебагу
   Serial.print("maxRotation - ");
   Serial.println(maxRotation);
   Serial.print("servoAngelStartTemp - ");
   Serial.println(servoAngelStartTemp);
+  // Потрібно для дебагу
 
   // меню початок тесту
-  // меню встановлення стартової температури startTemp + лоадінг
   show_text_in_menu("Start test", "", true);
   delay(1000);
-  show_text_in_menu("Set start temperature", "", true);
 
+  // поки цей функціонал не працює повноцінно, стартова температура зараз -10 С
   // обираємо стартову позицію сервомотора взалежності від температури
   switch (startTemp) {
     case 0:  // 0 Celcies
       servoAngelStartTemp = 20;
-      myservo.write(servoAngelStartTemp);
       break;
     case -10:  // -10 Celcies
       servoAngelStartTemp = calibration.getMinRotation();
-      myservo.write(servoAngelStartTemp);
       break;
     default:
-      myservo.write(servoAngelStartTemp);
+      Serial.println("default in switch startTemp");
   }
 
   // обираємо час проходження тесту в залежності від delta (5->15хв, 10->5хв, 20->3хв, 30->2хв)
+  // Від значення у servoTimeDeltaTemp залежить тривалість тесту у хв
+  // 1 хв -це приблизно зараз servoTimeDeltaTemp = 1500, залажить від калібровки яка ще недописана повноцінно
   switch (deltaTemp) {
     case 30:  // 30 Celcies/min (2хв)
       testTime = 2;
-      servoTimeDeltaTemp = 700;
-      //myservo.write(servoTimeDeltaTemp);
+      servoTimeDeltaTemp = 1500;
       break;
     case 20:  // 20 Celcies/min (3хв)
       testTime = 3;
-      servoTimeDeltaTemp = 1050;
-      //myservo.write(servoTimeDeltaTemp);
+      servoTimeDeltaTemp = 2250;
       break;
     case 10:  // 10 Celcies/min (5хв)
       testTime = 5;
-      servoTimeDeltaTemp = 1750;
-      //myservo.write(servoTimeDeltaTemp);
+      servoTimeDeltaTemp = 3750;
       break;
     case 5:  // 5 Celcies/min (15хв)
       testTime = 15;
-      servoTimeDeltaTemp = 5250;
-      //myservo.write(servoTimeDeltaTemp);
+      servoTimeDeltaTemp = 11250;
       break;
     default:
       testTime = 2;
-      servoTimeDeltaTemp = 700;
-      //myservo.write(servoTimeDeltaTemp);
+      servoTimeDeltaTemp = 1500;
   }
 
-  goToStartPositionServo();
-  // Затримка в дві секунди
-  delay(2000);
-
-  // меню старту тесту та відліку часу
+  //todo: меню старту тесту та відліку часу (додати пізніше)
   show_text_in_menu("Test in progress", "Time: " + String(testTime) + " min", true);
   Serial.println("servoTimeDeltaTemp - ");
   Serial.println(servoTimeDeltaTemp);
 
-
-  // запускаємо сервомотор з визначеними параметрами
-  // стартова позиція сервомотору в залежності віт стартової температури
-  // обираємо час проходження тесту в залежності від delta
-  for (pos = servoAngelStartTemp; pos <= maxRotation; pos += 1) {  // обертаємо сервомотор від 0 до 180 градусів
-    Serial.print("pos - ");
-    Serial.println(pos);
-    myservo.write(pos);                                            // задаємо кут серводвигуна
-    delay(servoTimeDeltaTemp);                                     // затримка для керування швидкістю повороту сервомотора
+// todo:тут костиль який треба виправити, і інтегрувати сюдт каліброску
+// pos = 60 це -10 С (тобто треба прокрутити сервомотор щоб було -10 С, якщо його не крутити то буде -17 С і мотор дрибіжить)
+  for (pos = 60; pos <= maxRotation; pos += 1) {  // обертаємо сервомотор від 0 до 180 градусів
+      Serial.print("pos - ");
+      Serial.println(pos);
+      myservo.write(pos);         // задаємо кут серводвигуна
+      delay(servoTimeDeltaTemp);  // затримка для керування швидкістю повороту сервомотора
+      // тут ідеально додати час тесту
   }
 
   // меню кінець тесту
@@ -299,13 +301,6 @@ void runTestServo(int startTemp, int deltaTemp) {
   redrawMenu();
 }
 
-// Видалити
-// void runTestServo() {
-//   for (pos = 0; pos <= 180; pos += 1) { // обертаємо сервомотор від 0 до 180 градусів
-//     myservo.write(pos);    // задаємо кут серводвигуна
-//     delay(50);             // затримка 50 мс для плавного руху
-//   }
-// }
 
 // -------------------------------------------------------------------------------
 // Повернення сервомотора у початкове положення
@@ -315,7 +310,7 @@ void goToStartPositionServo() {
   int minRotation = calibration.getMinRotation();
   int rotation = calibration.mapTemperatureToRotation(minRotation);
 
-  int currentRotation = myservo.read();  // Отримуємо поточний кут сервомотора
+  int currentRotation = minRotation;
   int targetRotation = minRotation;
 
   Serial.print("minRotation - ");
@@ -327,22 +322,6 @@ void goToStartPositionServo() {
   myservo.write(2);  // повертаємо сервомотор у початкове положення (0 градусів)
   delay(2000);
   myservo.write(rotation);  // повертаємо сервомотор у мін положення, що вказане у калібровкі (-10 С)
-
-  // Цей метод забезпечить плавний і поступовий поворот сервомотора до стартової позиції.
-  // // Якщо поточний кут більший за мінімальний, поступово зменшуємо кут
-  // if (currentRotation > targetRotation) {
-  //   for (int pos = currentRotation; pos >= targetRotation; pos--) {
-  //     myservo.write(pos);
-  //     delay(15);  // Затримка для плавного руху, можна змінювати для регулювання швидкості
-  //   }
-  // }
-  // // Якщо поточний кут менший за мінімальний, поступово збільшуємо кут
-  // else if (currentRotation < targetRotation) {
-  //   for (int pos = currentRotation; pos <= targetRotation; pos++) {
-  //     myservo.write(pos);
-  //     delay(15);  // Затримка для плавного руху
-  //   }
-  // }
 
   Serial.println("Сервомотор досяг мінімальної позиції");
 }
@@ -416,7 +395,7 @@ void redrawMenu() {
 void show_loading_menu_display() {
   for (int i = 1; i <= 3; i++) {
     clear_display();              // Очищення екрану
-    tft.setCursor(100, 100, 4);   // Встановлюємо курсор на екрані
+    tft.setCursor(50, 50, 4);   // Встановлюємо курсор на екрані
     tft.setTextColor(TFT_WHITE);  // Встановлюємо білий колір тексту
     tft.print("Loading");         // Виводимо текст "Loading"
 
@@ -449,38 +428,44 @@ void show_main_menu_display() {
   tft.println();
 
   tft.setTextColor(TFT_WHITE);  // Білий колір для тексту
-  tft.print("   Start Temp °C:  ");
-  tft.setTextColor(TFT_YELLOW);           // Жовтий колір для температури
-  tft.println(String(startTemperature));  // Виводимо початкову температуру
+  tft.print("   Start Temp:  ");
+  tft.setTextColor(TFT_YELLOW);         // Жовтий колір для температури
+  tft.print(String(startTemperature));  // Виводимо початкову температуру
+  tft.println(" C");
 
   tft.setTextColor(TFT_WHITE);  // Білий колір для тексту
-  tft.print("   Delta Temp °C :  ");
-  tft.setTextColor(TFT_YELLOW);           // Жовтий колір для температури
-  tft.println(String(deltaTemperature));  // Виводимо значення Delta Temperature
+  tft.print("   Delta Temp:  ");
+  tft.setTextColor(TFT_YELLOW);         // Жовтий колір для температури
+  tft.print(String(deltaTemperature));  // Виводимо значення Delta Temperature
+  tft.println(" C");
+
+  tft.setTextFont(2);
+  tft.setTextColor(TFT_WHITE);  // Білий колір для тексту
+  tft.print("                              v2024.1");
 }
 
 // -------------------------------------------------------------------------------
 // Виділення активного пункту меню
 // -------------------------------------------------------------------------------
 void showActiveMenuItem(int actieItem) {
-  Serial.println("Servo " + String(actieItem));
+  Serial.println("Active Menu Item " + String(actieItem));
 
   switch (actieItem) {
     case 1:
-      tft.fillRect(0, 55, 320, 26, TFT_BLUE);
+      tft.fillRect(0, 53, 320, 26, TFT_BLUE);
       break;
     case 2:
-      tft.fillRect(0, 82, 320, 26, TFT_BLUE);
+      tft.fillRect(0, 80, 320, 26, TFT_BLUE);
       break;
     case 3:
-      tft.fillRect(0, 109, 320, 26, TFT_BLUE);
+      tft.fillRect(0, 107, 320, 26, TFT_BLUE);
       break;
     case 4:
-      tft.fillRect(0, 136, 320, 26, TFT_BLUE);
+      tft.fillRect(0, 135, 320, 26, TFT_BLUE);
       break;
     default:
       Serial.println("Error: in main menu");
-      tft.fillRect(0, 55, 320, 27, TFT_BLUE);
+      tft.fillRect(0, 53, 320, 26, TFT_BLUE);
   }
 }
 
@@ -509,7 +494,7 @@ void show_text_in_menu(String text_1, String text_2, boolean isLoading) {
     tft.setTextColor(TFT_WHITE);  // Встановлюємо білий колір тексту
     tft.print(text_2);            // Виводимо текст "Loading"
 
-    // Затримка на 1 секунду
+    // Затримка на пів секунду
     delay(500);
   }
 }
